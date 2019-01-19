306c56fa8fa3e421d922de3d05a659eb1911e553
vivalaakam <viva.la.akam@gmail.com>
2019-01-19 15:36:30 +0300

    build

---
+++ build/precache-manifest.5d2edea1005fc4f700ba87d2ff2a37be.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed379fe3bcbcd8d37a4",
+    "url": "/static/css/main.b7c15607.chunk.css"
+  },
+  {
+    "revision": "1ed379fe3bcbcd8d37a4",
+    "url": "/static/js/main.1ed379fe.chunk.js"
+  },
+  {
+    "revision": "48155a963951fc403364",
+    "url": "/static/js/runtime~main.48155a96.js"
+  },
+  {
+    "revision": "118dfbf3d6bf2aa3de9d",
+    "url": "/static/js/2.118dfbf3.chunk.js"
+  },
+  {
+    "revision": "028c1f4dedd24b957b03",
+    "url": "/static/css/3.42ee807d.chunk.css"
+  },
+  {
+    "revision": "028c1f4dedd24b957b03",
+    "url": "/static/js/3.028c1f4d.chunk.js"
+  },
+  {
+    "revision": "d18baf22be030a80038c",
+    "url": "/static/js/4.d18baf22.chunk.js"
+  },
+  {
+    "revision": "cdcc372541b9929a9718",
+    "url": "/static/css/5.b53161e8.chunk.css"
+  },
+  {
+    "revision": "cdcc372541b9929a9718",
+    "url": "/static/js/5.cdcc3725.chunk.js"
+  },
+  {
+    "revision": "506c846118ee3fcaf87900ba0b90f00f",
+    "url": "/static/media/close.506c8461.svg"
+  },
+  {
+    "revision": "1e97968f3b7713d0f48ee339ed804530",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a28ab84495d4a1be3dc08264e9aa8a5.js"
+  "/precache-manifest.5d2edea1005fc4f700ba87d2ff2a37be.js"
 );
 
 workbox.clientsClaim();
